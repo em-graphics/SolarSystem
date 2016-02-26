e7a1c9806ed89c31aa47ada9bb2d4c3562c3a9ab
orangeem <orangeem@naver.com>
2016-02-26 14:03:34 -0500

    Update comment

--- Scripts/objects/point.js
+++ Scripts/objects/point.js
@@ -4,7 +4,7 @@ Live link : http://solarsystem-assignment2.azurewebsites.net
 Author : Eunmi Han(300790610)
 Date last Modified : Mar 26, 2016
 Program Description : Solar System (Mercury, Venus, Earth and Mars with Moon)
-Revision History :1.10
+Revision History :1.12
 
 Last Modified by Eunmi Han
 
